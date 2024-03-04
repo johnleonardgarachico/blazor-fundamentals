0bbc5b44dcfb99a618dcac026ed52688f06085d1
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-04 17:57:41 +0800

    added employee overview page

---
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employees { get; set; } = default!;
+
+        override protected void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
