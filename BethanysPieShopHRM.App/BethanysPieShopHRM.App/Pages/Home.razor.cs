77ac9f1501cf431a4773611f14f6f4ce0e11bc89
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-06 12:17:11 +0800

    added widgets

---
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/Home.razor.cs
@@ -0,0 +1,9 @@
+﻿using BethanysPieShopHRM.App.Components.Widgets;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class Home
+    {
+        public List<Type> Widgets { get; set; } = [typeof(EmployeeCountWidget), typeof(InboxWidget)];
+    }
+}
