5f9abdf3bf83d5e0bb8336019b6c619e63c2833d
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-06 18:52:03 +0800

    used http client in blazor app

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -1,19 +1,24 @@
 ﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieShopHRM.App.Pages
 {
     public partial class EmployeeOverview
     {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
         public List<Employee>? Employees { get; set; } = default!;
 
         private Employee? _selectedEmployee;
 
         private string Title = "Employee Overview";
 
-        override protected void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
-            Employees = MockDataService.Employees;
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
         }
 
         public void ShowQuickViewPopup(Employee selectedEmployee)
