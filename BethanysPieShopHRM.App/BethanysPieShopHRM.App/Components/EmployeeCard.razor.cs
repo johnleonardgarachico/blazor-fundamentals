fd706fd53993cf6c776eaf59d87a83652341cd1e
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-06 11:29:57 +0800

    used NavigationManager and RenderFragment

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Components/EmployeeCard.razor.cs
@@ -10,5 +10,18 @@ namespace BethanysPieShopHRM.App.Components
 
         [Parameter]
         public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        public void NavigateToDetails(Employee selectedEmployee)
+        {
+            // We can write more code here (as needed)
+
+            // That way we can have more control on how our navigation works
+            // For example, check if the user is allowed to navigate to the details page
+
+            NavigationManager.NavigateTo($"/employeedetail/{selectedEmployee.EmployeeId}");
+        }
     }
 }
