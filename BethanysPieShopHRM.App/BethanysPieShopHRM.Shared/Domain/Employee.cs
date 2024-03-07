77b63ce5ad68f62cb57d4130f8a30c5922b820ad
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-07 16:17:36 +0800

    added data annotations validations

--- BethanysPieShopHRM.App/BethanysPieShopHRM.Shared/Domain/Employee.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.Shared/Domain/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,15 @@ namespace BethanysPieShopHRM.Shared.Domain
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First Name is tooooooooooooo long.")]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50, ErrorMessage = "Last Name is tooooooooooooo long.")]
         public string LastName { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string Zip { get; set; } = string.Empty;
@@ -23,6 +30,7 @@ namespace BethanysPieShopHRM.Shared.Domain
         public bool Smoker { get; set; }
         public MaritalStatus MaritalStatus { get; set; }
         public Gender Gender { get; set; }
+        [StringLength(1000, ErrorMessage = "Comment length can't exceed 1000 characters.")]
         public string? Comment { get; set; }
         public DateTime? JoinedDate { get; set; }
         public DateTime? ExitDate { get; set; }
