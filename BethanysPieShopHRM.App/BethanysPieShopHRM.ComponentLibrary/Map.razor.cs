caf7c636e1373f177c71780a4c4d22ddcca1a642
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-09 14:39:22 +0800

    use razor class library for components

--- BethanysPieShopHRM.App/BethanysPieShopHRM.ComponentLibrary/Map.razor.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.ComponentLibrary/Map.razor.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace BethanysPieShopHRM.App.Components
+namespace BethanysPieShopHRM.ComponentLibrary
 {
     public partial class Map
     {
