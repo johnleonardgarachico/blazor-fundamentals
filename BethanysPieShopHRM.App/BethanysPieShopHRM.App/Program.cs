2f71b493ebfaabde567637ef8ebe51544a7d1ed3
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-09 21:42:45 +0800

    Added authentication in API calls

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
@@ -2,6 +2,7 @@ using BethanysPieShopHRM.App;
 using BethanysPieShopHRM.App.Services;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,11 +10,14 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => 
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client =>
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client =>
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddScoped<ApplicationState>(); // This is just the same with singleton
 
@@ -23,6 +27,7 @@ builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
 });
 
 await builder.Build().RunAsync();
