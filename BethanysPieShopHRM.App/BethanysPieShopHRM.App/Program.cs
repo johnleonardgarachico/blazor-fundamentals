938e1acfd1dd11fd317f7fb0c18855f4225abb4c
John Leonard GARACHICO <johnleonard.garachico@amadeus.com>
2024-03-09 21:03:41 +0800

    Added authentication with auth0

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
@@ -19,4 +19,10 @@ builder.Services.AddScoped<ApplicationState>(); // This is just the same with si
 
 builder.Services.AddBlazoredLocalStorage();
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
